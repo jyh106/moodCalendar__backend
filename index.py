5fe56c107fa4a49b31d37f42560f954756346c3b
jyh106 <jyh106@berkeley.edu>
2018-10-25 15:25:24 -0700

    receive day info when user submit mood form

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask import request
 
 app = Flask(__name__)
 CORS(app)
@@ -7,17 +8,26 @@ CORS(app)
 
 moods = ["fantastic", "good", "ok", 'meh', 'bad']
 
+yearMoods = {
+	# 'Jan1': {'mood': 'fantastic', 'note': 'love'},
+	# 'Jan2': {'mood': 'bad', 'note':'ds'}
+}
+
 @app.route('/')
 def index():
     return 'INDEX'
 
 @app.route('/get_mood', methods=['GET'])
 def get_mood():
-	return jsonify({
-        "mood": moods[0],
-        "squareID": "Jan1",
-        "note": "",
-    })
+	global yearMoods
+	return jsonify(yearMoods)
+
+@app.route('/store_mood', methods=['POST'])
+def store_mood():
+	global yearMoods
+ 	yearMoods[request.form.get('squareID')] = {'mood':request.form.get('mood'), 'note':request.form.get('note')}
+	return ""
+
 
 if __name__ == '__main__':
     app.run(debug = True)
